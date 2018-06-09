3772f24e85f284cd59bf50adcca2ef5d5b5e87e7
Sreejith Mani <Sreejith.Mani@gmail.com>
2018-06-09 11:54:35 +0200

    adding manual steps for nodejs container steps

---
+++ manual/steps.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+cd $HOME
+mkdir -p dockerstore
+cd dockerstore/
+git clone https://github.com/msreedata/hello-Node.git
+sudo yum -y install docker
+echo changing to hello-Node/
+pushd ..
+ls
+read -rsp $'Press any key to continue...\n' -n1 key
+
+systemctl status docker
+systemctl start docker
+systemctl status docker
+systemctl enable docker
+systemctl status docker
+read -rsp $'Press any key to continue...\n' -n1 key
+
+sudo docker build .
+less Dockerfile
+sudo docker image ls
+read -rsp $'Press any key to continue...\n' -n1 key
+
+sudo docker build . -t hellonode:v1
+sudo docker image ls
+read -rsp $'Press any key to continue...\n' -n1 key
+
+sudo docker run --rm -d -p 80:8080 --name hello1 hellonode:v1
+sudo docker ps -a
+#
+popd
