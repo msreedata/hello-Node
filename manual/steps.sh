40f00b2ba8c60e5a193d717768c0237e58385717
Sreejith Mani <Sreejith.Mani@gmail.com>
2018-06-09 12:00:55 +0200

    ..

--- manual/steps.sh
+++ manual/steps.sh
@@ -5,13 +5,12 @@ cd $HOME
 mkdir -p dockerstore
 cd dockerstore/
 git clone https://github.com/msreedata/hello-Node.git
-
+cd $HOME/dockerstore/hello-Node
 
 '
 
 cd $HOME/dockerstore/hello-Node
 sudo yum -y install docker
-pushd ..
 ls
 read -rsp $'Press any key to continue...\n' -n1 key
 
@@ -34,4 +33,3 @@ read -rsp $'Press any key to continue...\n' -n1 key
 sudo docker run --rm -d -p 80:8080 --name hello1 hellonode:v1
 sudo docker ps -a
 #
-popd
